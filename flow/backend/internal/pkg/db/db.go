21548d7916bf70cd914e7f19126e04207fec7818
Kimyeonho <kyh0703@nate.com>
2025-01-29 23:24:00 +0900

    🎨 [BACKEND] sql 쿼리 위치 조정

--- flow/backend/internal/pkg/db/db.go
+++ flow/backend/internal/pkg/db/db.go
@@ -1,19 +1,31 @@
 package db
 
 import (
+	"context"
 	"database/sql"
+	"time"
 
 	"github.com/kyh0703/flow/configs"
 	"github.com/kyh0703/flow/internal/core/domain/model"
 )
 
-func NewDB(config *configs.Config) model.DBTX {
+//go:embed schema.sql
+var ddl string
+
+func NewDB(config *configs.Config) (model.DBTX, error) {
 	db, err := sql.Open("sqlite", ":memory:")
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
-	return db
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if _, err := db.ExecContext(ctx, ddl); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 func NewQueries(db model.DBTX) *model.Queries {
